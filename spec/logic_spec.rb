79c3d3acc1c71d3ccd75f7f80e6fc13f160fcaec
ybadmus <yusif.badmus@gmail.com>
2021-02-16 21:42:51 +0000

    fix rubocop errors

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -1,11 +1,10 @@
 require_relative '../lib/logic'
 
 describe Logic do
-
   describe '#list_of_countries' do
     let(:logic) { Logic.new }
-    
-    it "returns the list of all countries" do
+
+    it 'returns the list of all countries' do
       expect(logic.list_of_countries.length).to eq(2586)
     end
   end
@@ -13,20 +12,20 @@ describe Logic do
   describe '#valid_country' do
     let(:logic) { Logic.new }
 
-    it "returns true if country is a valid country" do
-      expect(logic.valid_country("Ghana")).to be_truthy
+    it 'returns true if country is a valid country' do
+      expect(logic.valid_country('Ghana')).to be_truthy
     end
 
-    it "returns false if country is invalid" do
-      expect(logic.valid_country("Ghaaaaaa")).to be_falsey
+    it 'returns false if country is invalid' do
+      expect(logic.valid_country('Ghaaaaaa')).to be_falsey
     end
   end
 
   describe '#all_active_users' do
     let(:logic) { Logic.new }
 
-    it "returns all 256 active users" do
-      logic.active_country = "ghana"
+    it 'returns all 256 active users' do
+      logic.active_country = 'ghana'
       expect(logic.all_active_users.length).to eq(256)
     end
   end
@@ -34,18 +33,18 @@ describe Logic do
   describe '#active_users_in_range' do
     let(:logic) { Logic.new }
 
-    it "returns all users in a range" do
-      logic.active_country = "ghana"
+    it 'returns all users in a range' do
+      logic.active_country = 'ghana'
       expect(logic.active_users_in_range(10, 20).length).to eq(11)
     end
   end
 
   describe '#rank_by_username' do
     let(:logic) { Logic.new }
-    let(:user) { "ybadmus" }
+    let(:user) { 'ybadmus' }
 
-    it "returns user with the same username" do
-      logic.active_country = "ghana"
+    it 'returns user with the same username' do
+      logic.active_country = 'ghana'
       expect(logic.rank_by_username(user)[0][1]).to eq('ybadmus(Yusif Badmus)')
     end
   end
@@ -53,10 +52,9 @@ describe Logic do
   describe '#organizations_ranking' do
     let(:logic) { Logic.new }
 
-    it "returns 10 ranking of active companies using github" do
-      logic.active_country = "ghana"
+    it 'returns 10 ranking of active companies using github' do
+      logic.active_country = 'ghana'
       expect(logic.organizations_ranking.length).to eq(11)
     end
   end
-
 end
--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -1,29 +1,27 @@
 require_relative '../lib/scrapper'
 
 describe Scrapper do
-
   describe '#all_active_users' do
-    let(:scrapper) { Scrapper.new("ghana") }
+    let(:scrapper) { Scrapper.new('ghana') }
 
-    it "returns all 256 active users" do
+    it 'returns all 256 active users' do
       expect(scrapper.all_active_users.length).to eq(256)
     end
   end
 
   describe '#active_users_in_range' do
-  let(:scrapper) { Scrapper.new("ghana") }
+    let(:scrapper) { Scrapper.new('ghana') }
 
-    it "returns all users in a range" do
+    it 'returns all users in a range' do
       expect(scrapper.active_users_in_range(10, 20).length).to eq(20)
     end
   end
 
   describe '#organizations_ranking' do
-    let(:scrapper) { Scrapper.new("ghana") }
+    let(:scrapper) { Scrapper.new('ghana') }
 
-    it "returns 10 ranking of active companies using github" do
+    it 'returns 10 ranking of active companies using github' do
       expect(scrapper.organizations_ranking.length).to eq(11)
     end
   end
-
 end
